87af2f71659f3d61ad837441cab713ee5797644c
Rajdeep Tayde <rajdeeptayde@gmail.com>
2019-05-21 17:14:22 +0530

    Added Plugin Header

--- woocommerce-indian-currency-symbol.php
+++ woocommerce-indian-currency-symbol.php
@@ -2,29 +2,22 @@
 
 /**
  * woocommerce-indian-currency-symbol.php
- *
- * Copyright (c) 2014 "kento" Karim Rahimpur www.itthinx.com
- *
  * This code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
  *
  * This code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * This header and all notices must be kept intact.
- *
- * @author itthinx
+ * @author RajdeepTayde
  * @package woocommerce-product-generator
  * @since 1.0.0
  *
- * Plugin Name: WooCommerce Product Generator
- * Plugin URI: http://www.itthinx.com/
+ * Plugin Name: WooCommerce Indian Currency Symbol Using Webrupee
+ * Plugin URI: https://facebook.com/rajdeeptayde
  * Description: A sample product generator for WooCommerce.
  * Version: 1.1.1
- * Author: itthinx
- * Author URI: https://facebook
- * Donate-Link: http://www.itthinx.com
+ * Author: RajdeepTayde
+ * Author URI: https://facebook.com/rajdeeptayde
  * License: GPLv3
  */
