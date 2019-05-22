c7f36226e7637a114dbd197d971f8e0af9b5a3a7
rajdeeptayde <rajdeeptayde@gmail.com>
2019-05-22 11:22:06 +0530

    Completed the plugin

--- woocommerce-indian-currency-symbol.php
+++ woocommerce-indian-currency-symbol.php
@@ -21,3 +21,20 @@
  * Author URI: https://facebook.com/rajdeeptayde
  * License: GPLv3
  */
+
+
+add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10, 2);
+
+function change_existing_currency_symbol( $currency_symbol, $currency ) {
+	switch( $currency ) {
+	case 'INR': $currency_symbol = 'Rs. '; break;
+	}
+	return $currency_symbol;
+}
+
+
+add_action( 'wp_enqueue_scripts', 'wp_webrupee_js' );
+
+function wp_webrupee_js() {
+	wp_enqueue_style( 'wp-webrupee-script', plugin_dir_url( __FILE__ ).'css/webrupee.css' );
+}
